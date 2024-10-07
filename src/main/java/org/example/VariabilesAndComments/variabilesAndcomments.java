0e135ec620bbac99c057c51bcf9b99f776f94576
Lisa-sys326 <ghenciu.elizaveta@gmail.com>
2024-10-07 18:37:18 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/org/example/VariabilesAndComments/variabilesAndcomments.java
@@ -0,0 +1,40 @@
+package org.example.VariabilesAndComments;
+
+public class variabilesAndcomments {
+    public static void main(String[] args) {
+
+        // declare the primitive data type variables
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        //initialization of the above variables
+        falseBooleanVariable=false;
+        trueBooleanVariable=true;
+        //firstCharFromAscii=-128;
+        lowerLimitOfByteDataType=-128;
+        upperLimitOfByteDataType=127;
+        lowerLimitOfShortDataType=-32768;
+        upperLimitOfShortDataType=32767;
+        lowerLimitOfIntDataType=-2147483648;
+        upperLimitOfIntDataType=2147483647;
+        lowerLimitOfLongDataType=-456789098765435467L;
+        upperLimitOfLongDataType=456789098765435466L;
+        lowerLimitOfFloatDataType=-3.4E+38F;
+        upperLimitOfFloatDataType=3.4E+38F;
+        lowerLimitOfDoubleDataType=-1.7E+308d;
+        upperLimitOfDoubleDataType=1.7E+307d;
+    }
+}
